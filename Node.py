8c4d250f998e4ff74e296b5f79f404d82033986c
Simon <simon1240.se@gmail.com>
2023-04-11 20:49:05 +0200

    i cant draw a line help

---
+++ Node.py
@@ -0,0 +1,18 @@
+class Node():
+	def __init__(self,title,color=None,debug=False) -> None:
+		# self.logger = Logger(f"Node({title})",debug=debug)
+		self.data = {
+			"title":title,
+			"data":[],
+			"image":None,
+		}
+		self._children = []
+
+	def addDataField(self,datatype,data):
+		self.data["data"].append({datatype:data})
+
+	def removeDataField(self,datatype,data):
+		self.data["data"].remove({datatype:data})
+
+	def addImage(self,imagetype,imageUrl):
+		self.data["image"] = {imagetype:imageUrl}
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import contextlib
 from dearpygui.dearpygui import get_item_children,draw_line,get_item_pos,get_item_rect_size,node,node_attribute,mvNode_Attr_Static,add_text
-from os.path import isfile
 from math import sqrt
 
 class Link:
@@ -20,7 +20,7 @@ class Link:
 		return sqrt((p1[0] - p[0]) ** 2 + (p1[1] - p[1]) ** 2)
 
 	def draw(self) -> None:
-		self.link = draw_line(get_item_pos(self.node_1),get_item_pos(self.node_2),color=(0,0,0))
+		self.link = draw_line(get_item_pos(self.node_1),get_item_pos(self.node_2),color=(0,0,0),parent=self.editor)
 
 	def get_outer_node(self) -> int:
 		# returns the node that is further from the origin
@@ -36,6 +36,8 @@ class RelationalNodeUI:
 	def __init__(self,node_editor_id,min_link_length=100,max_link_length=300) -> int:
 		self.editor = node_editor_id
 		self.settings = {"max_link_length": max_link_length,"min_link_length": min_link_length}
+
+	def get_editor(self):
 		return self.editor
 
 	def visualize(self,root):
@@ -47,7 +49,7 @@ class RelationalNodeUI:
 		#enforce links
 
 	def createLinks(self,root):
-		self.links = [Link(root,child,self.editor) for child in todo]
+		self.links = [Link(root,child,self.editor) for child in root._children]
 		for node in (todo := root._children): # im not sure if this works
 			self.links.extend(Link(node,child,self.editor) for child in node._children)
 			todo.extend(node._children)
@@ -55,14 +57,60 @@ class RelationalNodeUI:
 	def get_editor_nodes(self) -> list[int]:
 		return get_item_children(self.editor)[children_node_index := 1]
 
+	def _show_info(self):
+		print(f"""
+{self.editor}
+{self.settings}
+{self.get_editor_nodes()}
+""")
+		with contextlib.suppress(Exception):
+			print(self.links)
+			for link in self.links:
+				print(link.node_1.data)
+				print(link.node_2.data)
+				print(link.origin)
+				print(link.get_length())
+
+	def draw(self):
+		for link in self.links:
+			link.draw()
+
 def createDPGNode(hdllnode,editor) -> int:
-	title = hdllnode.data["title"]
-	# description = "\n".join([line for key,line in list(hdllnode.data["data"])]) #idk maybe this will see
-	description = "\n".join([line if key != "default" else "" for key, line in list(hdllnode.data["data"])])
-	with node(label=title,parent=editor) as dpgNodeId:
-		with node_attribute(attribute_type=mvNode_Attr_Static):
-			add_text(description)
-	return dpgNodeId
+	if not hasattr(hdllnode,"transformedToDPGNode"):
+		title = hdllnode.data["title"]
+		description = ''.join([value for field in hdllnode.data['data'] for key, value in field.items() if key != dp._internal.is_root_node])
+		with node(label=title,parent=editor) as dpgNodeId:
+			if description != "":
+				with node_attribute(attribute_type=mvNode_Attr_Static):
+					add_text(description)
+		hdllnode.transformedToDPGNode = True
+		return dpgNodeId
 
 if __name__ == "__main__":
-	test = RelationalNodeUI(placeholder := 5)
+	from Node import Node
+	from Data import datapoints as dp
+
+	root = Node("ROOT")
+	root.addDataField(dp._internal.is_root_node,True)
+	testResult = Node("Example Result")
+	testResult.addDataField(dp.username.discord,"JCMS#0557")
+	root._children.append(testResult)
+	testResult2 = Node("Example Result 2")
+	testResult2.addDataField(dp.username.youtube,"JCMS_")
+	root._children.append(testResult2)
+	testResult3 = Node("Example Result 3")
+	testResult3.addDataField(dp.username.google,"JustCallMeSimon")
+	testResult2._children.append(testResult3)
+
+	from dearpygui import dearpygui as dpg
+	dpg.create_context()
+	dpg.create_viewport(title="Hello World", width=640, height=480)
+	dpg.setup_dearpygui()
+	with dpg.window(label="Example Window") as wnd:
+		rng = RelationalNodeUI(dpg.add_node_editor(parent=wnd))
+		dpg.add_button(label="info",callback=lambda: rng._show_info())
+		dpg.add_button(label="vis",callback=lambda: rng.visualize(root))
+		dpg.add_button(label="draw",callback=lambda: rng.draw())
+	dpg.set_primary_window(wnd,True)
+	dpg.show_viewport()
+	dpg.start_dearpygui()
