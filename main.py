d76dbdcbcbec65766d60fbc8db35934b4c6686b9
JustCallMeSimon <no>
2023-05-06 00:01:16 +0200

    awfwaf

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from dearpygui.dearpygui import get_item_children,get_item_pos,get_item_rect_size,node,node_attribute,mvNode_Attr_Static,add_text,set_item_pos,get_item_label,set_value
+from dearpygui.dearpygui import get_item_children,get_item_pos,get_item_rect_size,node,node_attribute,mvNode_Attr_Static,add_text,set_item_pos,get_item_label
 from math import sqrt
 from random import choice
-from threading import Thread
 
 import time
 
 class Link:
-	def __init__(self,node_1,node_2,editor) -> None:
+	def __init__(self,node_1,node_2,editor,is_child_link=False) -> None:
+		self.is_child_link = is_child_link
 		self.editor = editor
-		# self.origin = getItemMiddle(editor)
 		self.node_1 = createDPGNode(node_1,self.editor) # Always the Parent node (for now)
 		self.node_2 = createDPGNode(node_2,self.editor)
 
@@ -18,20 +17,22 @@ class Link:
 		try:
 			slope =  (y1-y)/(x1-x)
 		except ZeroDivisionError:
-			print("idk")
+			return
 		else:
 			if x1 < x and y1 < y or (x1 <= x or y1 >= y) and x1 < x and y1 > y:
-				new_y = (y1 + delta * slope) - get_item_rect_size(self.node_2)[1]/2
-				new_x = (x1 + delta) - get_item_rect_size(self.node_2)[0]/2
-			elif x1 > x and y1 < y or x1 > x and y1 > y:
 				new_y = (y1 - delta * slope) - get_item_rect_size(self.node_2)[1]/2
 				new_x = (x1 - delta) - get_item_rect_size(self.node_2)[0]/2
+			elif x1 > x and y1 < y or x1 > x and y1 > y:
+				new_y = (y1 + delta * slope) - get_item_rect_size(self.node_2)[1]/2
+				new_x = (x1 + delta) - get_item_rect_size(self.node_2)[0]/2
+			else:
+				return
 		set_item_pos(self.node_2,[new_x,new_y])
 
 	def get_length(self) -> int:
 		x,y = getItemMiddle(self.node_1)
 		x1,y1 = getItemMiddle(self.node_2)
-		return sqrt((x1-x)**2 + (y1-y)**2)
+		return round(sqrt((x1-x)**2 + (y1-y)**2))
 
 class RelationalNodeUI:
 	"""A Relational UI (Wrapper) specifically made for [Heimdall](https://hdll.jcms.dev) based on the node editor from [dearpygui](https://github.com/hoffstadt/DearPyGui/issues)
@@ -47,10 +48,6 @@ class RelationalNodeUI:
 		self.createLinks(root)
 		for node in self.get_editor_nodes():
 			self.randomiseNodePositions(node)
-		time.sleep(5)
-		while True:
-			self.links[0].change_length(30)
-			time.sleep(0.05)
 
 
 	def randomiseNodePositions(self,nodeId):
@@ -63,10 +60,16 @@ class RelationalNodeUI:
 		set_item_pos(nodeId,(x,y))
 
 	def createLinks(self,root) -> None:
+		childLists = [root._children]
 		self.links = [Link(root,child,self.editor) for child in root._children]
 		for node in (todo := root._children):
+			childLists.append(node._children)
 			self.links.extend(Link(node,child,self.editor) for child in node._children)
 			todo.extend(node._children)
+		for children in childLists:
+			self.links.extend([Link(children[i],children[i+1],self.editor,is_child_link=True) for i in range(len(children) - 1)])
+			if len(children) > 2:
+				self.links.append(Link(children[-1],children[0],self.editor,is_child_link=True))
 
 	def get_editor_nodes(self) -> list[int]:
 		return get_item_children(self.editor)[children_index := 1]
@@ -120,31 +123,33 @@ if __name__ == "__main__":
 	testResult = Node("Example Result")
 	testResult.addDataField(dp.username.discord,"JCMS#0557")
 	root._children.append(testResult)
-	# testResult2 = Node("Example Result 2")
-	# testResult2.addDataField(dp.username.youtube,"JCMS_")
-	# root._children.append(testResult2)
-	# testResult3 = Node("Example Result 3")
-	# testResult3.addDataField(dp.username.google,"JustCallMeSimon")
-	# testResult4 = Node("Example Result 3")
-	# testResult4.addDataField(dp.username.google,"JustCallMeSimon")
-	# testResult5 = Node("Example Result 3")
-	# testResult5.addDataField(dp.username.google,"JustCallMeSimon")
-	# testResult6 = Node("Example Result 3")
-	# testResult6.addDataField(dp.username.google,"JustCallMeSimon")
-	# testResult7 = Node("Example Result 3")
-	# testResult7.addDataField(dp.username.google,"JustCallMeSimon")
-	# testResult2._children.append(testResult3)
-	# testResult2._children.append(testResult4)
-	# testResult2._children.append(testResult5)
-	# testResult2._children.append(testResult6)
-	# testResult2._children.append(testResult7)
+	testResult2 = Node("Example Result 2")
+	testResult2.addDataField(dp.username.youtube,"JCMS_")
+	root._children.append(testResult2)
+	testResult3 = Node("Example Result 3")
+	testResult3.addDataField(dp.username.google,"JustCallMeSimon")
+	testResult4 = Node("Example Result 3")
+	testResult4.addDataField(dp.username.google,"JustCallMeSimon")
+	testResult5 = Node("Example Result 3")
+	testResult5.addDataField(dp.username.google,"JustCallMeSimon")
+	testResult6 = Node("Example Result 3")
+	testResult6.addDataField(dp.username.google,"JustCallMeSimon")
+	testResult7 = Node("Example Result 3")
+	testResult7.addDataField(dp.username.google,"JustCallMeSimon")
+	testResult2._children.append(testResult3)
+	testResult2._children.append(testResult4)
+	testResult2._children.append(testResult5)
+	testResult2._children.append(testResult6)
+	testResult2._children.append(testResult7)
 	from dearpygui import dearpygui as dpg
 	dpg.create_context()
-	dpg.create_viewport(title="Hello World", width=640, height=480)
+	dpg.create_viewport(title="Hello World", width=640, height=500)
 	dpg.setup_dearpygui()
 	with dpg.window(label="Example Window") as wnd:
 		rng = RelationalNodeUI(dpg.add_node_editor(parent=wnd,minimap=True))
 		dpg.add_button(parent=wnd,label="vis",callback=lambda: rng.visualize(root))
+		dpg.add_button(parent=wnd,label="all",callback=lambda: [link.change_length(1) for link in rng.links])
+		dpg.add_button(parent=wnd,label="all",callback=lambda: [link.change_length(-1) for link in rng.links])
 	dpg.set_primary_window(wnd,True)
 	dpg.show_viewport()
 	dpg.start_dearpygui()
