854608e5ded98da2b7efa26a5139579a5a3041c7
Simon <simon1240.se@gmail.com>
2023-05-29 21:35:05 +0200

    raw done. only optimise after a short break

--- main.py
+++ main.py
@@ -1,40 +1,24 @@
-from dearpygui.dearpygui import draw_line,draw_arrow,get_item_configuration,get_item_pos,get_item_rect_size,get_item_label,get_item_children,set_item_pos,node,node_attribute,mvNode_Attr_Static,add_text,delete_item
+from dearpygui.dearpygui import draw_arrow,get_item_pos,get_item_rect_size,get_item_children,set_item_pos,node,node_attribute,mvNode_Attr_Static,add_text,delete_item
 from math import sqrt
 import time
 import threading
 from random import choice
-from sympy import symbols, Eq, solve
+
+import math
 
 class Link:
-	def __init__(self,node_1,node_2,node_editor,child_link=False,root_link=False) -> None:
-		self.child_link = child_link
-		self.root_link = root_link
+	def __init__(self,node_1,node_2,node_editor) -> None:
 		self.node_1 = createDPGNode(node_1,node_editor) # Always the Parent node (for now)
 		self.node_2 = createDPGNode(node_2,node_editor)
 
 	def draw(self,drawList):
-		#TODO instead of drawing into the middle draw to the closest edge that is in direction of link. (idk how yet lmfao)
-		if self.child_link:
-			return draw_line(getItemMiddle(self.node_2),getItemMiddle(self.node_1),parent=drawList,color=(255,0,0),thickness=1)
-		elif self.root_link:
-			return draw_line(getItemMiddle(self.node_2),getItemMiddle(self.node_1),parent=drawList,color=(0,255,0),thickness=1)
-		else:
-			return draw_arrow(getItemMiddle(self.node_2),getItemMiddle(self.node_1),parent=drawList,color=(255,255,255),thickness=3)
+	 	#TODO instead of drawing into the middle draw to the closest edge that is in direction of link. (idk how yet lmfao)
 
-	def change_length(self,delta,dl=None):
-		# get positions
-		moveable_nodeID = [node for node in [self.node_2,self.node_1] if get_item_configuration(node)["draggable"]][0]
-		static_nodeID = self.node_2 if moveable_nodeID == self.node_1 else self.node_1
-		moveable_node_pos = get_item_pos(moveable_nodeID)
-		static_node_pos = get_item_pos(static_nodeID)
-		# calculate new positions
-		pos_if_delta_neg,pos_if_delta_pos = find_new_coords(moveable_node_pos[0],moveable_node_pos[1],static_node_pos[0],static_node_pos[1],delta)
-		# use new position
-		if (moveable_node_pos[0] > static_node_pos[0] and delta > 0 or moveable_node_pos[0] <= static_node_pos[0] and moveable_node_pos[0] < static_node_pos[0] and delta <= 0):
-			set_item_pos(moveable_nodeID,pos=pos_if_delta_pos)
-		elif (moveable_node_pos[0] > static_node_pos[0] or moveable_node_pos[0] < static_node_pos[0]):
-			set_item_pos(moveable_nodeID,pos=pos_if_delta_neg)
-		print("moved")
+		node_1_poses = generate_rectangle_points(self.node_1)
+		node_2_poses = generate_rectangle_points(self.node_2)
+
+		poses = find_closest_points(node_1_poses,node_2_poses)
+		return draw_arrow(poses[1],poses[0],parent=drawList,thickness=3)
 
 	def get_length(self) -> int:
 		x,y = getItemMiddle(self.node_1)
@@ -42,79 +26,146 @@ class Link:
 		return round(sqrt((x1-x)**2 + (y1-y)**2))
 
 class RelationalNodeUI:
-	"""A Relational UI (Wrapper) specifically made for [Heimdall](https://hdll.jcms.dev) based on the node editor from [dearpygui](https://github.com/hoffstadt/DearPyGui/issues)
+	"""A Relational UI (Wrapper) specifically made for [Heimdall](https://hdll.jcms.dev) based on the node editor from [dearpygui](https://github.com/hoffstadt/DearPyGui)
 	"""
 	def __init__(self,parent,width,height,) -> None:
+		self.width = width
+		self.height = height
 		with dpg.theme() as editor_theme:
 			with dpg.theme_component(dpg.mvAll):
 				dpg.add_theme_style(dpg.mvStyleVar_WindowPadding,0,0,category=dpg.mvThemeCat_Core)
-		self.editor = dpg.add_node_editor(parent=parent,width=width,height=height)
+				dpg.add_theme_style(dpg.mvNodeStyleVar_GridSpacing,0,category=dpg.mvThemeCat_Nodes)
+				dpg.add_theme_style(dpg.mvNodeStyleVar_NodePadding,6,4,category=dpg.mvThemeCat_Nodes)
+				dpg.add_theme_style(dpg.mvNodeStyleVar_NodeCornerRounding,0,category=dpg.mvThemeCat_Nodes)
+				dpg.add_theme_style(dpg.mvNodeStyleVar_NodeBorderThickness,0,category=dpg.mvThemeCat_Nodes)
+
+		self.editor = dpg.add_node_editor(parent=parent,width=self.width,height=self.height)
 		dpg.bind_item_theme(parent,editor_theme)
-		dpg.set_frame_callback(42,self.setup_draw_layer)
+		dpg.set_frame_callback(1,self.setup_draw_layer)
 
 	def setup_draw_layer(self):
-		with dpg.window(no_background=True,pos=dpg.get_item_pos(self.editor),width=dpg.get_item_rect_size(self.editor)[0],height=dpg.get_item_rect_size(self.editor)[1],no_move=True,no_title_bar=True,no_scrollbar=True,no_resize=True,max_size=(dpg.get_item_rect_size(self.editor)[0],dpg.get_item_rect_size(self.editor)[1]),horizontal_scrollbar=False,min_size=(dpg.get_item_rect_size(self.editor)[0],dpg.get_item_rect_size(self.editor)[1]),no_close=True,no_collapse=True,) as drawWindow:
-			self.drawList = dpg.add_drawlist(width=dpg.get_item_rect_size(self.editor)[0],height=dpg.get_item_rect_size(self.editor)[1])
+		with dpg.theme() as draw_window_theme:
+			with dpg.theme_component(dpg.mvAll):
+				dpg.add_theme_style(dpg.mvStyleVar_WindowPadding,0,0,category=dpg.mvThemeCat_Core)
+		with dpg.window(no_background=True,
+		pos=dpg.get_item_pos(self.editor),
+		width=self.width,
+		height=self.height,
+		no_move=True,
+		no_title_bar=True,
+		no_scrollbar=True,
+		no_resize=True,
+		max_size=(self.width,self.height),
+		horizontal_scrollbar=False,
+		min_size=(self.width,self.height),
+		no_close=True,
+		no_collapse=True) as drawWindow:
+			self.drawList = dpg.add_drawlist(width=self.width,height=self.height,pos=dpg.get_item_pos(self.editor))
+		dpg.bind_item_theme(drawWindow,draw_window_theme)
+
+
+	def startThreads(self):
+		self.drawThread = threading.Thread(target=self.drawLinks)
+		self.dragThread = threading.Thread(target=self.handleDragging)
+		self.drawThread.start()
+		self.dragThread.start()
 
 	def visualize(self,root) -> None:
 		self.createLinks(root)
-		self.randomiseNodePositions()
-		# self.drawThread = threading.Thread(target=self.drawLinks)
-		self.dragThread = threading.Thread(target=self.handleDragging)
-		# self.drawThread.start()
-		self.dragThread.start()
-		for i in range(10):
-			while True:
-				for link in self.links:
-					delta = 50 - link.get_length()
-					link.change_length(delta,dl=self.drawList)
-				break
-			# while True:
-		# 	for link in self.links:
-		# 		if link.root_link:
-		# 			if link.get_length() < 200:
-		# 				link.change_length(1)
-		# 		elif link.child_link:
-		# 			if link.get_length() < 100:
-		# 				link.change_length(1)
-		# 		elif link.get_length() < 100:
-		# 			link.change_length(1)
-		# 		elif link.get_length() > 200:
-		# 			link.change_length(-1)
 
-	def randomiseNodePositions(self):
-		for nodeId in self.get_editor_nodes():
-			origin = get_item_pos(self.editor)[0] + get_item_rect_size(self.editor)[0] / 2,get_item_pos(self.editor)[1] + get_item_rect_size(self.editor)[1] / 2
-			x = (origin[0] - (get_item_rect_size(nodeId)[0] / 2))
-			y = (origin[1] - (get_item_rect_size(nodeId)[1] / 2))
-			if get_item_label(nodeId) != "ROOT":
-				x += choice((-1,1))
-				y += choice((-1,1))
-			set_item_pos(nodeId,(x,y))
+		print(self.links)
+
+		# 1. Give Positions
+
+		# Center Root
+		set_item_pos(self.get_editor_nodes()[0],getItemMiddle(self.editor))
+
+		# Randomise other nodes
+		for node in self.get_editor_nodes()[1:]:
+			set_item_pos(node,(choice(range(self.width)),choice(range(self.height))))
+
+		# 2. Define parameters and variables
+		repulsive_force_constant = 3.0  # Adjust as needed
+		attractive_force_constant = 0.5  # Adjust as needed
+		damping_factor = 0.1  # Adjust as needed
+		max_displacement_threshold = 0.1  # Adjust as needed
+		max_iterations = 200  # Adjust as needed
+
+
+		# 3. Iterative force calculations and position updates
+		for _ in range(max_iterations):
+			max_displacement = 0.0
+
+			# Reset net force
+			net_forces = {node: [0.0, 0.0] for node in self.get_editor_nodes()}
+
+		# Calculate repulsive forces
+		for i, node1 in enumerate(self.get_editor_nodes()):
+			pos1 = get_item_pos(node1)
+			for node2 in self.get_editor_nodes()[i + 1:]:
+				pos2 = get_item_pos(node2)
+				displacement = [pos1[0] - pos2[0], pos1[1] - pos2[1]]
+				distance_squared = displacement[0] ** 2 + displacement[1] ** 2
+				distance = max(math.sqrt(distance_squared), 0.001)  # Avoid division by zero
+				force_magnitude = repulsive_force_constant / (distance_squared + 1e-6)  # Add a small value (epsilon)
+				force = [force_magnitude * (displacement[0] / distance),
+						force_magnitude * (displacement[1] / distance)]
+
+				net_forces[node1][0] += force[0]
+				net_forces[node1][1] += force[1]
+				net_forces[node2][0] -= force[0]
+				net_forces[node2][1] -= force[1]
+
+			# Calculate attractive forces
+			for connection in self.links:
+				node1, node2 = connection.node_1,connection.node_2
+				pos1 = get_item_pos(node1)
+				pos2 = get_item_pos(node2)
+				displacement = [pos1[0] - pos2[0], pos1[1] - pos2[1]]
+				distance = max(math.sqrt(displacement[0] ** 2 + displacement[1] ** 2), 0.001)  # Avoid division by zero
+				force_magnitude = attractive_force_constant * distance
+				force = [force_magnitude * (displacement[0] / distance),
+						force_magnitude * (displacement[1] / distance)]
+
+				net_forces[node1][0] -= force[0]
+				net_forces[node1][1] -= force[1]
+				net_forces[node2][0] += force[0]
+				net_forces[node2][1] += force[1]
+
+			# Update positions based on net forces
+			for node in self.get_editor_nodes():
+				displacement = net_forces[node]
+				displacement[0] *= damping_factor
+				displacement[1] *= damping_factor
+
+				current_pos = get_item_pos(node)
+				new_pos = [current_pos[0] + displacement[0], current_pos[1] + displacement[1]]
+				set_item_pos(node, new_pos)
+
+				# Calculate maximum displacement for convergence check
+				max_displacement = max(max_displacement, math.sqrt(displacement[0] ** 2 + displacement[1] ** 2))
+
+			# Check for convergence
+			if max_displacement < max_displacement_threshold:
+				break
 
 	def createLinks(self,root) -> None:
-		ListOfChildren = [root._children]
 		self.links = [Link(root,child,self.editor) for child in root._children]
 		todo = []
 		todo.extend(root._children)
 		for node in todo:
-			if len(node._children) >= 2:
-				ListOfChildren.append(node._children)
 			self.links.extend(Link(node,child,self.editor) for child in node._children)
-			self.links.append(Link(node,root,self.editor,root_link=True))
 			todo.extend(node._children)
-		for list in ListOfChildren:
-			self.links.extend(Link(list[index], list[index + 1], self.editor, child_link=True) for index in range(len(list) - 1))
 
 	def get_editor_nodes(self) -> list[int]:
 		return get_item_children(self.editor)[children_index := 1]
 
 	def drawLinks(self):
-		list_of_drawn_elements = [link.draw(self.drawList) for link in self.links]
-		time.sleep(1 / int(dpg.get_frame_rate()))
-		for l in list_of_drawn_elements:
-			dpg.delete_item(l)
-		return
+		while True:
+			list_of_drawn_elements = [link.draw(self.drawList) for link in self.links]
+			time.sleep(1 / int(dpg.get_frame_rate()))
+			for item in list_of_drawn_elements:
+				delete_item(item)
 
 	def handleDragging(self,isDragging = False):
 		while True:
@@ -132,7 +183,7 @@ class RelationalNodeUI:
 						while dpg.is_mouse_button_down(button=dpg.mvMouseButton_Left):
 							# implement relative mouse delta shits
 							dpg.set_item_pos(node,(dpg.get_drawing_mouse_pos()[0] - mouseDelta[0],dpg.get_drawing_mouse_pos()[1] - mouseDelta[1]))
-		return
+
 
 def getNodeByPosition(nodes,mousepos):
 	draggable_nodes = [node for node in nodes if dpg.get_item_configuration(node)["draggable"]]
@@ -164,30 +215,67 @@ def createDPGNode(hdllnode,editor) -> int:
 def getItemMiddle(item) -> tuple[float, float]:
 	return [get_item_pos(item)[0] + get_item_rect_size(item)[0] / 2,get_item_pos(item)[1] + get_item_rect_size(item)[1] / 2]
 
-def find_new_coords(moveable_x, moveable_y, static_x, static_y, delta):
-	# I dont really understand this math but smart ppl in discord.gg/math do.
-	# Calculate the line slope
-	start_time = time.time()
-	try:
-		line_slope = (static_y - moveable_y) / (static_x - moveable_x)
-	except ZeroDivisionError:
-		print(0)
-		if moveable_x == static_x:
-			return [moveable_x,moveable_y + delta]
-		elif moveable_y == static_y:
-			return [moveable_x + delta,moveable_y]
-	# Calculate the line y-intercept
-	line_intercept = moveable_y - line_slope * moveable_x
-	# Define the variables
-	new_x, new_y = symbols('new_x new_y')
-	# Define the equation of the line AB (y = mx + c)
-	line_eq = Eq(new_y, line_slope * new_x + line_intercept)
-	# Define the equation for the distance between points A and (x, y) ((x - old_x)^2 + (y - old_y)^2 = delta^2)
-	distance_eq = Eq((new_x - moveable_x) ** 2 + (new_y - moveable_y) ** 2, delta ** 2)
-	elapsed_time = time.time() - start_time
-	print(f"Calculation took {elapsed_time:.6f} seconds")
-	return solve((line_eq, distance_eq), (new_x, new_y))
+def generate_rectangle_points(node):
+	position = dpg.get_item_pos(node)
+	width = dpg.get_item_rect_size(node)[0]
+	height = dpg.get_item_rect_size(node)[1]
 
+	# Calculate the coordinates of the four corners
+	top_left = position
+	top_right = (position[0] + width, position[1])
+	bottom_left = (position[0], position[1] + height)
+	bottom_right = (position[0] + width, position[1] + height)
+
+	# Calculate the coordinates of the midpoints on each side
+	top_midpoint = (position[0] + width // 2, position[1])
+	bottom_midpoint = (position[0] + width // 2, position[1] + height)
+	left_midpoint = (position[0], position[1] + height // 2)
+	right_midpoint = (position[0] + width, position[1] + height // 2)
+
+	return [
+		# top_left,
+		# top_right,
+		# bottom_left,
+		# bottom_right,
+		top_midpoint,
+		bottom_midpoint,
+		left_midpoint,
+		right_midpoint,
+	]
+
+def find_closest_points(points1, points2):
+    closest_distance = float('inf')
+    closest_points = None
+
+    # Iterate over each pair of points from both lists
+    for point1 in points1:
+        for point2 in points2:
+            # Calculate the Euclidean distance between the two points
+            distance = math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+
+            # Update the closest distance and points if the current distance is smaller
+            if distance < closest_distance:
+                closest_distance = distance
+                closest_points = (point1, point2)
+
+    return closest_points
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################
 # Test case
 if __name__ == "__main__":
 	class dp:
@@ -211,7 +299,6 @@ if __name__ == "__main__":
 		def addDataField(self,datatype,data):
 			self.data["data"].append({datatype:data})
 
-########################################
 
 	root = Node("ROOT",_is_root=True)
 	testResult = Node("Example Result")
@@ -240,12 +327,9 @@ if __name__ == "__main__":
 	dpg.create_viewport(title="Hello World", width=1500, height=800)
 	dpg.setup_dearpygui()
 	with dpg.window(label="Example Window") as wnd:
-		rng = RelationalNodeUI(parent=wnd,width=600,height=600)
-		dpg.add_button(parent=wnd,label="repo",callback=lambda: rng.randomiseNodePositions())
+		rng = RelationalNodeUI(parent=wnd,width=1200,height=600)
 		dpg.add_button(parent=wnd,label="vis",callback=lambda: rng.visualize(root))
-		dpg.add_button(parent=wnd,label="away",callback=lambda: rng.links[0].change_length(10,rng.drawList))
-		dpg.add_button(parent=wnd,label="close",callback=lambda: rng.links[0].change_length(-10,rng.drawList))
-		dpg.add_button(parent=wnd,label="len",callback=lambda: print(rng.links[0].get_length()))
+		dpg.add_button(parent=wnd,label="fagw",callback=lambda: rng.startThreads())
 	dpg.set_primary_window(wnd,True)
 	dpg.show_viewport()
 	dpg.start_dearpygui()
